c451d2b3c23b5906f9ede084890d468b62ce96df
Markus <marskoy27@gmail.com>
2023-11-17 19:50:55 +0200

    Задание 4

---
+++ 2312.py
@@ -0,0 +1,13 @@
+import colorama
+
+
+print(dir(colorama))
+
+
+print(colorama.Fore)
+print(colorama.Back)
+print(colorama.Style)
+
+# Важні методи
+print(colorama.init)
+print(colorama.init(autoreset=True))
